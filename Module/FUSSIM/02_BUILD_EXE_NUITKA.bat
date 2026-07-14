d95ec3845fb09e78e72340c212ce92befc694022
nagimasa81-cmyk <nagimasa81@gmail.com>
2026-07-15 02:06:20 +0900

    Delete Module/FUSSIM directory

--- Module/FUSSIM/02_BUILD_EXE_NUITKA.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python -m pip install -r requirements.txt nuitka ordered-set zstandard
-python -m nuitka --standalone --onefile --enable-plugin=pyside6 --include-package=matplotlib --windows-console-mode=disable --output-filename=FUS_Treatment_Replay.exe fus_treatment_replay.py
-pause
